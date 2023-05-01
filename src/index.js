a8c1f692efd5ff938a5462565562efe2e06fce18
Miroslav Ljubicic <127101426+mrkyns@users.noreply.github.com>
2023-05-01 12:24:26 +0200

    created navigation using Router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import DataContext from "./context/DataContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <DataContext>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </DataContext>
 );
