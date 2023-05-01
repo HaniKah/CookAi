9aae9cef2ada42924f0a3bb9c0693d2ee46111e9
Miroslav Ljubicic <127101426+mrkyns@users.noreply.github.com>
2023-05-01 10:26:06 +0200

    changed accesstoken and id

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Creators from "./components/Creators";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Creators />
+    </div>
+  );
 }
 
 export default App;
