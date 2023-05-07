d682acf256c8e7d7f3ae04b4513770076d97d42f
NoCookieFace <72047611+NoCookieFace@users.noreply.github.com>
2023-05-07 14:27:11 +0200

    Filter
    String-categories finished.
    Starting with calories Filter.

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,5 +1,6 @@
-import { useState, useEffect, createContext } from "react";
+import { useState, useEffect, createContext, useContext } from "react";
 import { client } from "../client";
+import { FilterContext } from '../context/FilterContext';
 
 export const DataContext = createContext();
 
@@ -10,6 +11,29 @@ export default function DataContextProvider(props) {
   const [submitted, setSubmitted] = useState(false);
   const [array, setArray] = useState([]);
   const [loading, setLoading] = useState(false);
+  const conditionsForRecipeFetch = "&random=true&field=dietLabels&field=healthLabels&field=ingredients&field=cuisineType&field=mealType&field=dishType"
+  const { dietSelectedFilters, healthSelectedFilters, cuisineSelectedFilters, mealSelectedFilters, dishSelectedFilters } = useContext(FilterContext);
+
+  const dietSelectedFiltersModified = dietSelectedFilters.map(i => '&diet=' + i);
+  const healthSelectedFiltersModified = healthSelectedFilters.map(i => '&health=' + i);
+  const cuisineSelectedFiltersModified = cuisineSelectedFilters.map(i => '&cuisine=' + i);
+  const mealSelectedFiltersModified = mealSelectedFilters.map(i => '&meal=' + i);
+  const dishSelectedFiltersModified = dishSelectedFilters.map(i => '&dish=' + i);
+
+  // if(healthSelectedFiltersModified.length > 0){
+  //   healthSelectedFiltersModified.unshift("&health=")
+  // }
+  // if(cuisineSelectedFiltersModified.length > 0){
+  //   cuisineSelectedFiltersModified.unshift("&cuisine=")
+  // }
+  // if(mealSelectedFiltersModified.length > 0){
+  //   mealSelectedFiltersModified.unshift("&meal=")
+  // }
+  // if(dishSelectedFiltersModified.length > 0){
+  //   dishSelectedFiltersModified.unshift("&dish=")
+  // }
+
+  console.log(healthSelectedFiltersModified)
 
   // fatching data from Contetful for displaying creators
   useEffect(() => {
@@ -31,7 +55,12 @@ export default function DataContextProvider(props) {
           url +
           array.join("%20") +
           `&app_id=${process.env.REACT_APP_ID}` +
-          `&app_key=${process.env.REACT_APP_KEY}`;
+          `&app_key=${process.env.REACT_APP_KEY}` +
+          dietSelectedFiltersModified.join("") +
+          healthSelectedFiltersModified.join("") +
+          cuisineSelectedFiltersModified.join("") +
+          mealSelectedFiltersModified.join("") +
+          dishSelectedFiltersModified.join("");
         const recipesResponse = await fetch(url);
         const recipesData = await recipesResponse.json();
         setLoading(false);
